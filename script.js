d0c1553500fef9d9e743c017d8472a4f5fd62633
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-06 14:28:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 window.onload = function() {
-    // Get the form element
+    const R = 6371000; // Earth radius in meters
+    const C = 2 * Math.PI * R; // Earth's circumference in meters
+
     const form = document.getElementById('calc-form');
 
-    // Listen for form submission
     form.addEventListener('submit', function(event) {
-        // Prevent the form from being submitted normally
         event.preventDefault();
 
-        // Get the distance input by the user
-        const l0 = document.getElementById('l0').value;
+        const h1 = parseFloat(document.getElementById('h1').value);
+        const L0 = parseFloat(document.getElementById('l0').value);
 
-        // Perform the Earth curvature calculations here
-        // For now, we'll just log the input value
-        console.log('L0 distance:', l0);
+        // Calculate d1, d2, and d0
+        const d1 = Math.sqrt(2 * h1 * R);
+        const d2 = L0 - d1;
+        const d0 = d1 + d2;
 
-        // TODO: Add code here to calculate d1, d2, and h1
+        // Calculate the fraction of the Earth's circumference that BX represents
+        const BOX_fraction = d2 / C;
 
-        // TODO: Add code here to display the results on the webpage
+        // Calculate the angle at BOX in radians
+        const BOX_angle = 2 * Math.PI * BOX_fraction;
+
+        // Calculate BC and OC
+        const BC = R / Math.sin(BOX_angle);
+        const OC = R / Math.cos(BOX_angle);
+
+        // Calculate XC
+        const XC = OC - R;
+
+        // Display the results
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = `
+            <p>d1: ${d1} meters</p>
+            <p>d2: ${d2} meters</p>
+            <p>d0: ${d0} meters</p>
+            <p>h2: ${XC} meters</p>
+        `;
     });
 };
